dd0e37f44a07e44cf5c34ac7e867ec164e39475e
Luv <153155546+Luvkapur7@users.noreply.github.com>
2024-01-14 13:03:21 +0000

    lecture1-code

---
+++ Lecture1/script.js
@@ -0,0 +1,8 @@
+const accountID = "112233"
+let accountName = "luv"
+let accountCity = "delhi"
+
+/* Changing values */
+accountName = "ajay"
+accountCity = "gurugram"
+console.table([accountID, accountName, accountCity]);
