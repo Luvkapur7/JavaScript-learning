0ffcdfe6eff0f3f53a90110f9f282ed25e9846f3
luv kapur <luvkapur4@gmail.com>
2024-01-16 22:14:17 +0530

    completed lecure 3

--- Lecture 3/test-2.js
+++ Lecture 3/test-2.js
@@ -1,10 +1,22 @@
 //  conversion and operations 
 
 let score = "33"
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score) //coversion into number
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 //conversions in js is somewhat a risky language which can carry forward errors so be carefull
 
+// *****operations*****
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "luv"
+let str2 = " kapur"
+let str3 = str1 + str2;
+// console.log(str3)
+
+console.log ("1" + (2 + 2))
