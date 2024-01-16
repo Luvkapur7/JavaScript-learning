6388e3508469c82ebf927815b9567a48469fb4ef
luv kapur <luvkapur4@gmail.com>
2024-01-16 20:24:04 +0530

    did some changes and some styuding

---
+++ Lecture 3/test.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js code as new version a good practice use this always
+
+let Name = "luv" //this is a string
+let age = 18 //this is a number
+let isLoggedIn = true //this is a boolean
+
+//null this is a stand alone value
+
+// undefined is when there is no value provided
+
+// symbol is used to find uniquness
+
+console.log(typeof isLoggedIn);
+//seeing type of
