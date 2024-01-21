ed79e59a4a86d0706e3eb088f8213eec36f4167a
luv kapur <luvkapur4@gmail.com>
2024-01-21 17:48:56 +0530

    heap and stack js

---
+++ Lecture 5/script.js
@@ -0,0 +1,18 @@
+let myName = "luv kapur"
+
+let anotherName = myName
+anotherName = "luv"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "abc@gmail.com",
+    upi: "user@sbi"
+}
+
+let userTwo = userOne;
+userTwo.email = "luv@google.com"
+
+console.log(userTwo)
+console.log(userOne)
